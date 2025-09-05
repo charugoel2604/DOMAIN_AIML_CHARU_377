eee0ca92ed53324182284cba1d78a9b4277aab2e
goelcharu2604 <goelcharu2006@gmail.com>
2025-09-05 22:40:54 +0530

    Swapping of two numbers

---
+++ program4.py
@@ -0,0 +1,7 @@
+a=int(input("Enter the value of a : "))
+b=int(input("Enter the value of b : "))
+c=a
+a=b
+b=c
+print("After swapping value of a is : {} ".format(a))
+print("After swapping value of b is : {} ".format(b))
